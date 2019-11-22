e367c112e692e3b1feb71c10e23e53f7951d6800
Stephenalexngo <51846517+Stephenalexngo@users.noreply.github.com>
2019-11-22 14:12:02 +0800

    Minor Changes in CRUD function

--- action.php
+++ action.php
@@ -2,42 +2,6 @@
 session_start();
 $ip_add = getenv("REMOTE_ADDR");
 include "db.php";
-if(isset($_POST["category"])){
-	$category_query = "SELECT * FROM categories";
-	$run_query = mysqli_query($con,$category_query) or die(mysqli_error($con));
-	echo "
-		<div class='nav nav-pills nav-stacked'>
-			<li class='active'><a href='#'><h4>Categories</h4></a></li>
-	";
-	if(mysqli_num_rows($run_query) > 0){
-		while($row = mysqli_fetch_array($run_query)){
-			$cid = $row["cat_id"];
-			$cat_name = $row["cat_title"];
-			echo "
-					<li><a href='#' class='category' cid='$cid'>$cat_name</a></li>
-			";
-		}
-		echo "</div>";
-	}
-}
-if(isset($_POST["brand"])){
-	$brand_query = "SELECT * FROM brands";
-	$run_query = mysqli_query($con,$brand_query);
-	echo "
-		<div class='nav nav-pills nav-stacked'>
-			<li class='active'><a href='#'><h4>Brands</h4></a></li>
-	";
-	if(mysqli_num_rows($run_query) > 0){
-		while($row = mysqli_fetch_array($run_query)){
-			$bid = $row["brand_id"];
-			$brand_name = $row["brand_title"];
-			echo "
-					<li><a href='#' class='selectBrand' bid='$bid'>$brand_name</a></li>
-			";
-		}
-		echo "</div>";
-	}
-}
 if(isset($_POST["page"])){
 	$sql = "SELECT * FROM products";
 	$run_query = mysqli_query($con,$sql);
@@ -62,8 +26,6 @@ if(isset($_POST["getProduct"])){
 	if(mysqli_num_rows($run_query) > 0){
 		while($row = mysqli_fetch_array($run_query)){
 			$pro_id    = $row['product_id'];
-			$pro_cat   = $row['product_cat'];
-			$pro_brand = $row['product_brand'];
 			$pro_title = $row['product_title'];
 			$pro_price = $row['product_price'];
 			$pro_image = $row['product_image'];
@@ -83,23 +45,14 @@ if(isset($_POST["getProduct"])){
 		}
 	}
 }
-if(isset($_POST["get_seleted_Category"]) || isset($_POST["selectBrand"]) || isset($_POST["search"])){
-	if(isset($_POST["get_seleted_Category"])){
-		$id = $_POST["cat_id"];
-		$sql = "SELECT * FROM products WHERE product_cat = '$id'";
-	}else if(isset($_POST["selectBrand"])){
-		$id = $_POST["brand_id"];
-		$sql = "SELECT * FROM products WHERE product_brand = '$id'";
-	}else {
-		$keyword = $_POST["keyword"];
-		$sql = "SELECT * FROM products WHERE product_keywords LIKE '%$keyword%'";
-	}
+if(isset($_POST["search"])){
+	
+	$keyword = $_POST["keyword"];
+	$sql = "SELECT * FROM products WHERE product_keywords LIKE '%$keyword%'";
 	
 	$run_query = mysqli_query($con,$sql);
 	while($row=mysqli_fetch_array($run_query)){
 			$pro_id    = $row['product_id'];
-			$pro_cat   = $row['product_cat'];
-			$pro_brand = $row['product_brand'];
 			$pro_title = $row['product_title'];
 			$pro_price = $row['product_price'];
 			$pro_image = $row['product_image'];
